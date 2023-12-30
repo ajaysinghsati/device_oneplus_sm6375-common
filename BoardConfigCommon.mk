ff2b15809a92d4d222263063e0fc8e371afac21c
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2023-12-31 03:42:10 +0530

    add framework_compatibility_matrix.xml
    Fix some errors by adding this $(COMMON_PATH)/framework_compatibility_matrix.xml \

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,6 +77,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/derp/config/device_framework_matrix.xml
